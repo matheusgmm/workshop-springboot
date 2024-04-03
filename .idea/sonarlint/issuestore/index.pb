0d02b0bee4a8db74e1ed36ae6a92c2b043acddde
Matheus Muccio <matheus.muccio@rodojacto.com.br>
2024-04-03 11:20:48 -0300

    User entity and resource

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,7 @@ r
 Bsrc/main/java/com/muccio/springcourse/SpringCourseApplication.java,5/8/5818b2eb7f4e0bd76bb08ac755549ae95dfa32f4
 7
 pom.xml,4/4/442292b8a7efeabbe4cc176709b833b1792140ec
+h
+8src/main/java/com/muccio/springcourse/entities/User.java,f/5/f57f0da7e71cd5219e29382049d8e271dba72f94
+q
+Asrc/main/java/com/muccio/springcourse/resources/UserResource.java,2/9/29d203e3e934fee01cabb94487c9ea0f165ff51d
---
+++ src/main/java/com/muccio/springcourse/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.muccio.springcourse.resources;
+
+import com.muccio.springcourse.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "999999", "12425");
+        return ResponseEntity.ok().body(u);
+    }
+
+
+}
